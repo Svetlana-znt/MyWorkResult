427c02fb1007f95a0d141ff99ffccca8d3e59003
Svetlana <svetpv2007@yandex.ru>
2023-04-04 16:24:21 +0500

    Создание цикла с условием

--- Program.cs
+++ Program.cs
@@ -23,3 +23,16 @@ do
     for (int i = 0; i < arr1.Length; i++)
         Console.WriteLine("arr1[{0}] = {1}", i, arr1[i]);
         Console.ReadKey();
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+    {
+        int count = 0;
+        for (int i = 0; i < array1.Length; i++)
+            {
+                if(array1[i].Length <= 3)
+                    {
+                        array2[count] = array1[i];
+                        count++;
+                    }
+            }
+    }
