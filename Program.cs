dcc2eb68862c4e989ae8fe98846650d343f4d517
Svetlana <svetpv2007@yandex.ru>
2023-04-04 16:16:58 +0500

    Создание цикла ввода строк

--- Program.cs
+++ Program.cs
@@ -2,3 +2,20 @@
 int count;
 string s;
 string[] arr;
+
+Console.WriteLine("Enter strings:");
+count = 0;
+arr1 = new string[count];
+do
+    {
+        s = Console.ReadLine();
+        if (s!="")
+           {
+                count++;
+                arr = new string[count];
+                for (int i = 0; i < count-1; i++)
+                    arr[i] = arr1[i];
+                arr[count - 1] = s;
+                arr1 = arr;
+            }
+    } 
